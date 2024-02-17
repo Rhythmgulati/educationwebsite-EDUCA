dccb343970e262cd5aa1dc93ed02b9ba519d0cef
Rhythmgulati <109156350+Rhythmgulati@users.noreply.github.com>
2024-02-17 16:32:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,55 @@ menuBtn.onclick = () =>{
     menuBtn.classList.toggle("fa-times");
     navBar.classList.toggle("active");
 };
+
+var swiper = new Swiper('.course-slider', {
+    spaceBetween:20,
+    loop: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+
+  var swiper = new Swiper('.reviews-slider', {
+    spaceBetween:20,
+    loop: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
